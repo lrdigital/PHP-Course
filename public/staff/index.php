e11bd50ce103d50cb7a1255e200cd95dfa5f61a2
Liam Rourke <web@liamrourke.digital>
2023-06-19 11:57:22 +0100

    initial commit

---
+++ public/staff/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
+</head>
+<body>
+    <header>
+        <h1>Liam's Website: Staff Area</h1>
+    </header>
+
+    <navigation>
+        <ul>
+            <li>
+                <a href="index.php">Menu</a>
+            </li>
+        </ul>
+    </navigation>
+
+    <div id="content">
+    </div>
+
+    <footer>
+        &copy; <?php echo date('Y') ?> Liam's Website
+    </footer>
+</body>
+</html>
