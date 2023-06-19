b3ac0227895f56dc172c5ebb74570bc6ad18806e
Liam Rourke <web@liamrourke.digital>
2023-06-19 12:20:09 +0100

    Passed in page title to header & created a default if N/A

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -1,9 +1,13 @@
+<?php
+    if(!isset($page_title)) { $page_title = 'Staff Area'; }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Liam's Site - <?php echo $page_title; ?></title>
 
     <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
 </head>
